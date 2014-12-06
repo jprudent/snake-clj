b2262c77be9a708d26db5b9ffe0c0eec32d45d28
Jérôme Prudent <jprudent@gmail.com>
2014-12-06 11:26:51 +0100

    refactoring

--- src/snake_clj/core.clj
+++ src/snake_clj/core.clj
@@ -14,40 +14,42 @@
 (defn- head [snake]
   (peek snake))
 
-(defn- move-right [world [x y] heading]
-  (let [[xr yr] (condp = heading
-                  :right [0 1]
-                  :left [0 -1]
-                  :up [1 0]
-                  :down [-1 0])
-        y (mod (+ y yr) (arity-y world))
-        x (mod (+ x xr) (arity-x world))]
+(defn- move-direction [world [x y] heading transformation-vector]
+  (let [[xt yt] (heading transformation-vector)
+        y (mod (+ y yt) (arity-y world))
+        x (mod (+ x xt) (arity-x world))]
     [x y]))
 
-(defn- move-left [world [x y] heading]
-  (let [[xr yr] (condp = heading
-                  :right [0 -1]
-                  :left [0 1]
-                  :up [-1 0]
-                  :down [1 0])
-        y (mod (+ y yr) (arity-y world))
-        x (mod (+ x xr) (arity-x world))]
-    [x y]))
+(defn- move-right [world head heading]
+  (move-direction world head heading {:right [0 1]
+                                      :left  [0 -1]
+                                      :up    [1 0]
+                                      :down  [-1 0]}))
+
+(defn- move-left [world head heading]
+  (move-direction world head heading {:right [0 -1]
+                                      :left  [0 1]
+                                      :up    [-1 0]
+                                      :down  [1 0]}))
 
 (defn- is-tail? [snake p]
   (some #(= p %) snake))
 
-(defn- direction-of [{:keys [world snake heading]} direction]
+(defn- direction-of [{:keys [world snake heading]} move-direction]
   (let [head (head snake)
-        new-head-position (direction world head heading)]
+        new-head-position (move-direction world head heading)]
     (if (is-tail? snake new-head-position)
       :tail
       (get-at world new-head-position))))
 
-(defn right-of [snake-state]
+(defn right-of
+  "Peek what's on right"
+  [snake-state]
   (direction-of snake-state move-right))
 
-(defn left-of [snake-state]
+(defn left-of
+  "Peek what's on left"
+  [snake-state]
   (direction-of snake-state move-left))
 
 (defn ahead-of [])
--- test/snake_clj/core_test.clj
+++ test/snake_clj/core_test.clj
@@ -2,20 +2,7 @@
   (:require [clojure.test :refer :all]
             [snake-clj.core :refer :all]))
 
-#_(deftest test-right-of
-  (let [aggregate {:world-dimension 2
-                   :world           [nil :wall
-                                     nil :apple]}]
-    (testing "right-of when snake one cell long"
-      (is (= [1 1 :wall] (right-of (assoc aggregate :snake [[0 1]])))))
-    #_(testing "right-of when snake head is not on last column"
-      (is (= [1 1 :wall] (right-of {:snake [[0 0] [0 1]]
-                              :world world})))
-      (is (= [0 0] (right-of {:snake [[1 1] [1 0]]
-                                    :world world}))))))
-
 (deftest test-right-of
-
   (is (= :tail (right-of (snake-state [[nil]] [[0 0]] :up)))
       "Right of a 1 cell world is always the tail of a snake")
   (is (= :apple (right-of (snake-state [[nil :apple]] [[0 0]] :up)))
@@ -33,7 +20,6 @@
         "Can see right when going left")))
 
 (deftest test-left-of
-
   (is (= :tail (left-of (snake-state [[nil]] [[0 0]] :up)))
       "Left of a 1 cell world is always the tail of a snake")
   (is (= nil (left-of (snake-state [[nil :apple]] [[1 0]] :up)))
