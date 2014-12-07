8e3e499bc945798fa215343f74d6ef514c5c9400
Jérôme Prudent <jprudent@gmail.com>
2014-12-07 20:55:26 +0100

    check for alive in commands

--- src/snake_clj/commands.clj
+++ src/snake_clj/commands.clj
@@ -4,35 +4,33 @@
     [snake-clj.events :as evt]
     [snake-clj.db :as db]))
 
-
 (defn start-game! [id seed]
   (db/store! (evt/game-started id seed)))
-
-(defn- move [direction-of event-factory id]
-  )
-
-
-
 
 (defn go-ahead! [id]
   (apply db/store!
          (if-let [snake (db/load-aggregate id)]
-           (let [turned-evt (evt/gone-ahead id)
-                 hit-smthing-evt (cond
-                                   (wall? (ahead-of snake)) (evt/hit-wall id)
-                                   (apple? (ahead-of snake)) (evt/ate-apple id)
-                                   (tail? (ahead-of snake)) (evt/ate-tail id))]
-             (if (nil? hit-smthing-evt)
-               [turned-evt]
-               [turned-evt hit-smthing-evt]))
+           (if (:alive? snake)
+             (let [turned-evt (evt/gone-ahead id)
+                   hit-smthing-evt (cond
+                                     (wall? (ahead-of snake)) (evt/hit-wall id)
+                                     (apple? (ahead-of snake)) (evt/ate-apple id)
+                                     (and (tail? (ahead-of snake))) (evt/ate-tail id))]
+               (if (nil? hit-smthing-evt)
+                 [turned-evt]
+                 [turned-evt hit-smthing-evt])))
            [])))
 
 (defn turn-left! [id]
   (println "turn left")
-  (apply db/store! [(evt/turned-left id)])
-  (go-ahead! id))
+  (if (:alive? (db/load-aggregate id))
+    (do
+      (apply db/store! [(evt/turned-left id)])
+      (go-ahead! id))))
 
 (defn turn-right! [id]
   (println "turn right")
-  (apply db/store! [(evt/turned-right id)])
-  (go-ahead! id))
+  (if (:alive? (db/load-aggregate id))
+    (do
+      (apply db/store! [(evt/turned-right id)])
+      (go-ahead! id))))
